f00f25aa6b197618aea3256eb00d0028a013d124
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-03-27 15:05:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    new_string = array.join(", ") 
+    new_string = array.join(", ")
     new_array = new_string.split(" ")
     
   end
